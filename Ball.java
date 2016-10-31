4ceefe8c1ce6686bf18e581a073aeb25f5fd5906
Ryan Jennings <ryanajennings11098@gmail.com>
2016-10-31 17:06:05 -0400

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -1,26 +1,97 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /// @brief A class encapsulating a state for our simulation
 ///
 /// A Ball is the State of the simulator. It contains an intrinsic mass. It
 /// contains position and velocity data. It contains operations to access, modify,
 /// and integrate the state.
 public class Ball {
-  private Vector pos, vel; ///< Position/velocity/windforce
-  private double mass;     ///< Mass
-  private double dynamics;
+  private Vector pos, vel;   ///< Position/velocity
+  private double mass;       ///< Mass constant
+  private double drag;       ///< Drag constant
+  private double elasticity; ///< Elasticity constant
+  private double friction;   ///< Friction constant
+  private boolean rest;      ///< Simulate agent (true)
 
-  /// @brief Construor to initialize position/velocity to (0,0) and mass to m
-  /// @param m Mass
-  public Ball(double m, double d) {
-    dynamics = d;
-    mass = m;
-    pos = new Vector();
-    vel = new Vector();
-
+  /// @brief Copy constructor
+  /// @param a Ball
+  public Ball(Ball a) {
+    pos = new Vector(a.pos);
+    vel = new Vector(a.vel);
+    mass = a.mass;
+    drag = a.drag;
+    elasticity = a.elasticity;
+    friction = a.friction;
+    rest = a.rest;
+  }
+  /// @brief Constructor from Scanner
+  /// @param s Scanner
+  public Ball(Scanner s) {
+    rest = false;
+    boolean done = false;
+    while(s.hasNextLine() && !done) {
+      String line = s.nextLine();
+      Scanner l = new Scanner(line);
+      String tag = l.next();
+      switch(tag) {
+        case "Mass":
+          System.out.println("Reading mass");
+          mass = l.nextDouble();
+          break;
+        case "Drag":
+          System.out.println("Reading drag");
+          drag = l.nextDouble();
+          break;
+        case "Elasticity":
+          System.out.println("Reading elasticity");
+          elasticity = l.nextDouble();
+          break;
+        case "Friction":
+          System.out.println("Reading friction");
+          friction = l.nextDouble();
+          break;
+        case "Position":
+          System.out.println("Reading position");
+          pos = new Vector(l);
+          break;
+        case "Velocity":
+          System.out.println("Reading velocity");
+          vel = new Vector(l);
+          break;
+        case "~Ball":
+          System.out.println("Reading ~Ball");
+          done = true;
+          break;
+        default:
+          throw new InputMismatchException("Unknown tag " + tag);
+      }
+      l.close();
+    }
   }
 
-  public Vector getVel() {return vel;}
-  public double getMass() {return mass;}
-  public double getDyn() {return dynamics;}
+  /// @return Mass of the ball
+  public double mass() {return mass;}
+  /// @return Drag of the ball
+  public double drag() {return drag;}
+  /// @return Elasticity of the ball
+  public double elasticity() {return elasticity;}
+  /// @return Friction of the ball
+  public double friction() {return friction;}
+  /// @return If the ball is resting
+  public boolean rest() {return rest;}
+  /// @return Set resting state of ball
+  public void rest(boolean r) {rest = r;}
+  /// @return Position of the ball
+  public Vector pos() {return pos;}
+  /// @return Velocity of the ball
+  public Vector vel() {return vel;}
+  /// @brief Set position of the ball
+  /// @param p Position
+  public void pos(Vector p) {pos = new Vector(p);}
+  /// @brief Set velocity of the ball
+  /// @param v Velocity
+  public void vel(Vector v) {vel = new Vector(v);}
 
   /// @brief Apply force to the ball for a small time step using Euler
   ///        Integration
@@ -30,24 +101,13 @@ public class Ball {
   /// x = x + t*v;
   /// v = v + t*a;
   public void applyForce(Vector force, double t) {
-    Vector acc = force.scalarMultiply(1./mass); //mass of 1
-    pos.add(vel.scalarMultiply(t));
-    vel.add(acc.scalarMultiply(t));
-
-    //Bounce off bottom/top of box
-    if(Math.abs(pos.Y()) > 50) {
-      vel.Y(-vel.Y());
-    }
-  
-    //Bounce off the sides of the box
-    if(Math.abs(pos.X()) > 50) {
-      vel.X(-vel.X());
+    Vector acc = force.multiply(1./mass); //F = m*a meaning a = F/m
+    pos.addeq(vel.multiply(t));
+    vel.addeq(acc.multiply(t));
   }
-
-}
 
   /// @brief Draw the ball as a circle
   public void draw() {
-    GUI.drawCircle(pos.X(), pos.Y(), 2);
+    GUI.drawCircle(pos.x(), pos.y(), 2);
   }
 }
