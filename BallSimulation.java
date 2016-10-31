4ff9fee8a1b1b77008e17a9de264be1e461d1704
Ryan Jennings <ryanajennings11098@gmail.com>
2016-10-31 17:06:31 -0400

    Update BallSimulation.java

--- BallSimulation.java
+++ BallSimulation.java
@@ -1,19 +1,37 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.Exception;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /// @brief This program simulates the motion of a bouncing ball in a vacuum.
 public class BallSimulation {
   public static void main(String args[]) {
-    if(args.length != 4) {
-      System.out.println("Usage: java BallSimulation <mass> <time step> <gravity magnitude in down-y direction> <dynamics>");
+    if(args.length != 1) {
+      System.out.println("Usage: java BallSimulation <file>");
       System.exit(1);
     }
 
-    // Input mass, time, gravity
-    double m = Double.parseDouble(args[0]);
-    double t = Double.parseDouble(args[1]); //0.#
-    double g = Double.parseDouble(args[2]); // - down + up
-    double d = Double.parseDouble(args[3]); // bigger -> wind affects more
-
     // Make a simulator on the parameters and simulate
-    Simulator simulator = new Simulator(m, t, g, d);
-    simulator.simulate();
+    try {
+      Scanner s = new Scanner(new File(args[0]));
+      Simulator simulator = new Simulator(s);
+      simulator.simulate();
+      System.exit(0);
+    }
+    catch(FileNotFoundException e) {
+      System.out.println("Cannot find file " + args[0]);
+      System.exit(1);
+    }
+    catch(InputMismatchException e) {
+      System.out.println("Misformatted simulation file");
+      System.out.println(e.getMessage());
+      System.exit(1);
+    }
+    catch(Exception e) {
+      System.out.println("Unknown error");
+      System.out.println(e.getMessage());
+      System.exit(1);
+    }
   }
 }
