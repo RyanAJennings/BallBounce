ae4c4d5f93b918065ee4b6086188ca00ae0cc696
Ryan Jennings <ryanajennings11098@gmail.com>
2016-10-31 17:21:12 -0400

    Create Obstacle.java

---
+++ Obstacle.java
@@ -0,0 +1,17 @@
+/*
+import java.io.FileNotFoundException;
+import java.io.File;
+
+public class Obstacle{
+
+  private Vector[][] points;
+  private Vector[][] normals;
+  private edge[] edges;
+  
+  public static void obstacle(String file){
+    ...
+    draw();
+  }
+  
+}
+*/
