66051b76a2a09df941d8ac8bcaeaa45cfec7d07b
Ryan Jennings <ryanajennings11098@gmail.com>
2016-11-08 11:03:02 -0500

    Update Obstacle.java

--- Obstacle.java
+++ Obstacle.java
@@ -1,17 +1,25 @@
-/*
+
 import java.io.FileNotFoundException;
 import java.io.File;
+import java.util.Scanner;
 
 public class Obstacle{
 
-  private Vector[][] points;
-  private Vector[][] normals;
+  private Vector[] points;
+  private Vector[] normals;
   private edge[] edges;
   
   public static void obstacle(String file) throws FileNotFoundException{
-    ...
+    Scanner s = new Scanner(new File(file));
+    int numPoints = s.nextInt;
+    for(int i = 0; i < numPoints; i++){
+      j = 0;
+      while(s.hasNextDouble()){
+        points[i][j] = s.nextDouble;
+      }
+     }
     draw();
   }
   
 }
-*/
+
